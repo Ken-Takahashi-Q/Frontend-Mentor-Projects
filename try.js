7e9c9b536629ba7420124e03bd65fc9db56ef96d
Yathip Kenichi Takahashi <ken.yathip@gmail.com>
2023-02-01 21:57:54 +0700

    update

--- try.js
+++ try.js
@@ -23,6 +23,27 @@ function ans(S) {
 
 console.log(ans(S))
 
+//Interview
+function fn(P1, P2) {
+    var num = [];
+    var result = [];
+    for (let i = 0; i<=P1; i++) {
+        num.push(i)
+    }
+
+    for (let i = 0; i<num.length; i++) {
+        if (P2 === 'even' && i%2 == 0) {
+            result.push(num[i])
+        }
+        else if (P2 === 'odd' && i%2 != 0) {
+            result.push(num[i])
+        }
+    }
+    return result
+}
+console.log(fn(5, 'even'))
+
+
 //1
 window.setInterval(cb, 1000)
 
