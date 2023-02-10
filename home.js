82c7cbdb74f7258476bdec04e7460b9c13823fa7
Yathip Kenichi Takahashi <ken.yathip@gmail.com>
2023-02-10 22:54:02 +0700

    update Home

---
+++ home.js
@@ -0,0 +1,4 @@
+const btnBeginner = document.getElementById("btn_beginner");
+const btnMedium = document.getElementById("btn_medium");
+const btnHard = document.getElementById("btn_hard");
+const btnMaster = document.getElementById("btn_master");
