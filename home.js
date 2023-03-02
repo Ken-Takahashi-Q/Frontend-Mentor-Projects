f91b920d66dd368e2b4e26a53cad167483ce81cc
Yathip Kenichi Takahashi <ken.yathip@gmail.com>
2023-03-02 08:53:36 +0700

    updated Home

--- home.js
+++ home.js
@@ -1,4 +1,23 @@
-const btnBeginner = document.getElementById("btn_beginner");
-const btnMedium = document.getElementById("btn_medium");
-const btnHard = document.getElementById("btn_hard");
-const btnMaster = document.getElementById("btn_master");
+const btnBeginner = document.querySelector("#btn_beginner");
+const btnMedium = document.querySelector("#btn_medium");
+const btnHard = document.querySelector("#btn_hard");
+const btnMaster = document.querySelector("#btn_master");
+
+const cards = document.querySelectorAll('.card');
+
+btnBeginner.addEventListener('click', filterCards);
+btnMedium.addEventListener('click', filterCards);
+btnHard.addEventListener('click', filterCards);
+btnMaster.addEventListener('click', filterCards);
+
+function filterCards(event) {
+  const level = event.target.dataset.level;
+
+  cards.forEach((card) => {
+    if (card.querySelector(`.difficulty-${level}`)) {
+      card.classList.add('show');
+    } else {
+      card.classList.remove('show');
+    }
+  });
+}
